a2b60d1661199c9c8d26ea866587d79a9ca64a86
Lutz <lutz_klein@brown.edu>
2020-10-03 21:28:56 +0200

    credentials not part of the repository anymore

--- src/app/store/db.ts
+++ src/app/store/db.ts
@@ -1,7 +1,9 @@
 // import PouchDB from 'pouchdb';
 import PouchDB from 'pouchdb';
-export const username="apikey-68840a2a2f6b49beb183cc9018db9d15" // API Key
-export const password="3fe5600a035e8113cfd4fd9f22bb6ecd8cb8a5b8" // API Password
+// @ts-ignore
+import {secret_username, secret_password} from './credentials.ts'
+export const username=secret_username
+export const password=secret_password
 // export const url="93934ced-34cd-4218-9ca4-382e3e94cb39-bluemix.cloudantnosqldb.appdomain.cloud"
 export const url="93934ced-34cd-4218-9ca4-382e3e94cb39-bluemix.cloudant.com"
 // export const remoteCouch = 'https://admin:admin@192.168.178.31:5984';
